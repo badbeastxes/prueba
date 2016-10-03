009decc8ddc04b2406dd8b10ccba44d97c67485a
badbeastxes <mingarrolunaraul.guaradam@gmail.com>
2016-10-03 20:18:52 +0200

    Add files via upload

---
+++ codigo.js
@@ -0,0 +1,11 @@
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+var nombre=prompt("Introduce tu nombre","Aquí tu nombre");
+var edad=prompt("Hola "+nombre+",introduce ahora tu edad");
+
+
+var mayor=Number(edad)-18;
+alert(nombre+" \nTienes "+edad+" años\nHace "+Number(mayor)+" que eres mayor de edad");
